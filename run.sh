6580b7abbab8dcc0dbbdf1a07537d1dd2418b830
Singh, Ravi <raviksingh@paypal.com>
2017-10-06 22:13:16 +0530

    script changes

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ SCRIPT_FOLDER="$( cd "$( dirname "$0" )"; pwd -P )"
 DOCKER_VM=`basename $SCRIPT_FOLDER`
 
 # Search for local built docker container. This will be given priority to run.
-DOCKER_IMAGE=`docker image ls | grep $DOCKER_VM | grep -v "/$DOCKER_VM" | awk -F' ' '{print $1}'`
+DOCKER_IMAGE=`docker image ls | grep $DOCKER_VM | grep latest | grep -v "/$DOCKER_VM" | awk -F' ' '{print $1}'`
 if [ -z $DOCKER_IMAGE ]
 then
    # If local docker container not found search for pulled docker container.
-   DOCKER_IMAGE=`docker image ls | grep "/$DOCKER_VM" | awk -F' ' '{print $1}'`
+   DOCKER_IMAGE=`docker image ls | grep "/$DOCKER_VM" | grep latest | awk -F' ' '{print $1}'`
 fi
 
-if [ ! -z $DOCKER_IMAGE ]
+if [ ! -z "$DOCKER_IMAGE" ]
 then
    SCRIPT="docker run -it $DOCKER_IMAGE"
 
